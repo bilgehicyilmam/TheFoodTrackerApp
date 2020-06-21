4665e1a807195c691ef66823a89b83ef5434d695
Bilge Hicyilmam <bilge.hicyilmam@boun.edu.tr>
2020-06-21 15:50:14 +0200

    fix login handler error

--- food_tracker_api/scripts/buildAndDeploy.sh
+++ food_tracker_api/scripts/buildAndDeploy.sh
@@ -1,6 +1,6 @@
 mvn clean package
 
-scp -i "foackernew.pem" ./target/food_tracker_api-0.0.1.jar ec2-user@ec2-3-134-80-99.us-east-2.compute.amazonaws.com:deploy/food_tracker_api-0.0.1.jar
+scp -i "foodtrackernew.pem" ./target/food_tracker_api-0.0.1.jar ec2-user@ec2-3-134-80-99.us-east-2.compute.amazonaws.com:deploy/food_tracker_api-0.0.1.jar
 
 echo "Uploading to server..."
 ssh ec2-user@ec2-3-134-80-99.us-east-2.compute.amazonaws.com -i "foodtrackernew.pem" 'bash -s' < ./scripts/_updateAndRestart.sh
